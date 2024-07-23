405a864ffa37e9012e749a6f8b7bd43f3a459721
thiagosimi10 <thiagosimi10@hotmail.com>
2024-07-22 21:19:10 -0300

    Adicionando Graficos

--- data.js
+++ data.js
@@ -3,30 +3,24 @@ var data = [{
     country: "USA",
     value: 2025,
     visits: 725,
-    icon: "https://www.amcharts.com/wp-content/uploads/flags/united-states.svg",
 
 }, {
     country: "China",
     value: 1882,
     visits: 602,
-    icon: "https://www.amcharts.com/wp-content/uploads/flags/china.svg",
 
 }, {
     country: "Japan",
     value: 1809,
     visits: 509,
-    icon: "https://www.amcharts.com/wp-content/uploads/flags/japan.svg",
 
 }, {
     country: "Germany",
     value: 1322,
     visits: 322,
-    icon: "https://www.amcharts.com/wp-content/uploads/flags/germany.svg",
 
 }, {
     country: "UK",
     value: 1122,
     visits: 625,
-    icon: "https://www.amcharts.com/wp-content/uploads/flags/united-kingdom.svg",
-
 },];
